5f3f169cb80300cb34c58a27e3b89ad46bbe5daf
Ramiz Masic <masic.ramiz.7@gmail.com>
2026-03-14 05:14:26 +0100

    feat(01-02): update AppModule and configure NestJS

---
+++ apps/api/src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+  });
+  
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+  }));
+  
+  await app.listen(3000);
+  console.log('API running on http://localhost:3000');
+}
+bootstrap();
